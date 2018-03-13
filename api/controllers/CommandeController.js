4df259259cbbb68b4c580b1894373dda454cb1ab
root <root@debian-8-1x.etna.local>
2018-03-13 10:20:23 +0100

    Reorganisation

---
+++ api/controllers/CommandeController.js
@@ -0,0 +1,26 @@
+/**
+ * CommandeController
+ *
+ * @description :: Server-side logic for managing commandes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+    createCommande: function(req, res) {
+        Commande.create( { acheteur: req.param('acheteur'), nbRepas: req.param('nbRepas'), nomRepas: req.param('nomRepas'), date: req.param('date'), prixTotal: req.param('prixTotal') }, function(err,created){
+            if(!err) {
+                console.log('Article créé : '+ created.nomRepas +', ayant pour ID : '+created.id+'.');
+            }
+            else {
+                return err;
+            }
+        });
+    },
+
+    getCommandes: function(req, res) {
+        Commande.find({}, function(err, found){
+            res.view( 'commandes', {commandes: found} );
+        });
+    }
+};
+
