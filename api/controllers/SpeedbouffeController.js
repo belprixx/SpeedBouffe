d7ac71409cba52ba9fdd398a04fd101ef67c108a
amo <amo@opBox>
2018-04-03 16:21:34 +0200

    debut connexion mongo

---
+++ api/controllers/SpeedbouffeController.js
@@ -0,0 +1,12 @@
+/**
+ * SpeedbouffeController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  
+
+};
+
