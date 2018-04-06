0310d870efc80056f3204104329379e0a25ca1e8
Romain Chassaigne <chassa_r@etna-alternance.net>
2018-04-06 10:21:49 +0200

    Modify docker-compose

--- image/sailsjs/entrypoint.sh
+++ image/sailsjs/entrypoint.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
-if [ ! -d ".temp" ]; then
-    mkdir .temp
-    chown -R sailsjs:sailsjs /home/sailsjs
+if [ ! -d "/root/speedbouffe/.temp" ]; then
+    mkdir /root/speedbouffe/.temp
+    chown -R root:root /root/speedbouffe/.tmp
 fi
 
-npm install --silent -g sails grunt bower
+# npm install --silent -g sails grunt bower
 
-yarn install
+cd /root/speedbouffe && \
+yarn install && \
 yarn add sails-disk --save
+
+# npm install sails-mongo
+
+sails generate api speedbouffe
 
 sails lift --dev --silly
